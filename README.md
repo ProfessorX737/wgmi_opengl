fd8d0eb07af538d0438850516fb284973068af0d
Xavier Poon <xavierpoon737@gmail.com>
2022-01-17 21:41:50 +1100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# OpenGL CMake Setup
+
+This project uses submodules for its dependencies so,
+if cloning for the first time:
+1. Accept any pending merge requests on gitlab
+2. `git clone <repo> --recursive`
+
+If already cloned, to update submodules do the following:c
+1. `git pull`
+2. `git submodule update --init --remote --recursive`
---
+++ apps/spinning_wgmi/src/main.cpp
@@ -0,0 +1,91 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void process_input(GLFWwindow* window);
+
+// settings
+const char* win_title = "Example";
+const unsigned int scr_width = 800;
+const unsigned int scr_height = 600;
+
+int main() {
+	// glfw: initialize and configure
+	// ------------------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// glfw window creation
+	// --------------------
+	GLFWwindow* window = glfwCreateWindow(scr_width, scr_height, win_title, NULL, NULL);
+	if (window == NULL) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// ############################### TODO ################################
+	// create shaders and shapes here
+	// #####################################################################
+
+	// render loop
+	// -----------
+	while (!glfwWindowShouldClose(window)) {
+		// input
+		// -----
+		process_input(window);
+
+		// render
+		// ------
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// ############################### TODO ################################
+		// update your shaders and render your shapes here
+		// #####################################################################
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react
+// accordingly
+// ---------------------------------------------------------------------------------------------------------
+void process_input(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
